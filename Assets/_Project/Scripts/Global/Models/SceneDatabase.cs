dbf6e42b5eda006c5f76e453ce492cde0fa73c52
Yaser Syafa <syafayaser@gmail.com>
2026-01-29 08:58:12 +0700

    Add Scene Database

---
+++ Assets/_Project/Scripts/Global/Models/SceneDatabase.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+namespace ColorOrCrash.Global.Models
+{
+    [CreateAssetMenu(fileName = "SceneDatabase", menuName = "Scriptable Objects/SceneDatabase")]
+    public class SceneDatabase : ScriptableObject
+    {
+        [Scene]
+        public string MainMenuScene;
+        [Scene]
+        public string GameScene;
+    }
+}
