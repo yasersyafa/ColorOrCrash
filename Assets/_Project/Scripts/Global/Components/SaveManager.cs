20c4ec62253427378162e3b4c497b0ad552eeeda
Yaser Syafa <syafayaser@gmail.com>
2026-01-31 14:26:36 +0700

    Update SaveManager

--- Assets/_Project/Scripts/Global/Components/SaveManager.cs
+++ Assets/_Project/Scripts/Global/Components/SaveManager.cs
@@ -7,17 +7,39 @@ namespace ColorOrCrash
     public class SaveManager : MonoBehaviour, IGameService
     {
         public static string keyTutorial = "tutorial";
-        public static bool HasTutorial()
-        {
-           return PlayerPrefs.HasKey(keyTutorial) && PlayerPrefs.GetInt(keyTutorial) == 1; 
-        }  
+        public static string keyScore = "score";
+
         private void Awake()
         {
             ServiceLocator.Register<SaveManager>(this);
         }
 
+#region public API methods
+        
+        public static bool HasTutorial()
+        {
+           return PlayerPrefs.HasKey(keyTutorial) && PlayerPrefs.GetInt(keyTutorial) == 1; 
+        }
+
+        public static void SetHighScore(int score)
+        {
+            if(PlayerPrefs.HasKey(keyScore))
+            {
+                bool highScore = score > PlayerPrefs.GetInt(keyScore);
+                if(highScore)
+                {
+                    PlayerPrefs.SetInt(keyScore, score);
+                }
+            }
+            else
+            {
+                PlayerPrefs.SetInt(keyScore, score);
+            }
+        }
+
         public static void Save() => PlayerPrefs.SetInt(keyTutorial, 1);
 
+#endregion
         void OnDestroy()
         {
             ServiceLocator.Unregister<SaveManager>();
